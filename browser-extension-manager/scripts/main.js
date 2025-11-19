e66c75b814712eba5cf8c619a0bbbd08754b7203
Victor Hugo <hugo.victoor77@gmail.com>
2025-11-19 06:37:34 -0300

    Create filter functions

--- browser-extension-manager/scripts/main.js
+++ browser-extension-manager/scripts/main.js
@@ -2,10 +2,51 @@ import { ExtensionItem } from "./extension-item.js";
 import { loadJsonFile } from "./utils.js";
 
 class App {
-    async init() {
-        const data = await loadJsonFile('../data.json');
+    filteredData = [];
+    data = [];
 
-        data.forEach(ext => {    
+    async init() {
+        this.data = await loadJsonFile('../data.json');
+        this.filteredData = this.data || [];
+        console.log({data: this.data, f: this.filteredData});
+        
+        const filterAllBtn = document.querySelector('#filter-all');
+        filterAllBtn.addEventListener('click', () => this.filterAll());
+
+        const filterActiveBtn = document.querySelector('#filter-active');
+        filterActiveBtn.addEventListener('click', () => this.filterActive());
+
+        const filterInactiveBtn = document.querySelector('#filter-inactive');
+        filterInactiveBtn.addEventListener('click', () => this.filterInactive());
+        
+        this.filteredData.forEach(ext => {    
+            new ExtensionItem(ext.name, ext.isActive, ext.logo);
+        });
+    }
+
+    filterAll() {
+        const list = document.querySelector('#extensions-list');
+        list.innerHTML = '';
+
+        this.data.forEach(ext => {    
+            new ExtensionItem(ext.name, ext.isActive, ext.logo);
+        });
+    }
+
+    filterActive(){
+        const list = document.querySelector('#extensions-list');
+        list.innerHTML = '';
+        
+        this.data.filter(ext => ext.isActive).forEach(ext => {    
+            new ExtensionItem(ext.name, ext.isActive, ext.logo);
+        });
+    }
+
+     filterInactive(){
+        const list = document.querySelector('#extensions-list');
+        list.innerHTML = '';
+        
+        this.data.filter(ext => !ext.isActive).forEach(ext => {    
             new ExtensionItem(ext.name, ext.isActive, ext.logo);
         });
     }
