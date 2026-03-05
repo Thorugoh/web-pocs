8e88fcd79dd464336e76a8d832f5025f0839ec6d
Victor Hugo <hugo.victoor77@gmail.com>
2026-03-05 09:10:33 -0300

    dynamicly show contacts

--- react-router-address-book/app/root.tsx
+++ react-router-address-book/app/root.tsx
@@ -7,10 +7,16 @@ import {
   isRouteErrorResponse,
 } from "react-router";
 import type { Route } from "./+types/root";
-
+import { getContacts } from "./data";
 import appStylesHref from "./app.css?url";
 
-export default function App() {
+export async function clientLoader(){
+  const contacts = await getContacts();
+  return { contacts };
+}
+
+export default function App({ loaderData }: Route.ComponentProps) {
+  const { contacts } = loaderData;
   return (
     <>
       <div id="sidebar">
@@ -32,12 +38,30 @@ export default function App() {
         </div>
         <nav>
           <ul>
-            <li>
-              <Link to={`/contacts/1`}>Your Name</Link>
-            </li>
-            <li>
-              <Link to={`/contacts/2`}>Your Friend</Link>
-            </li>
+              {
+                contacts.length ? (
+                  <ul>
+                    {contacts.map((contact) => (
+                      <li key={contact.id}>
+                        <Link to={`contacts/${contact.id}`}>
+                          {contact.first || contact.last ? (
+                            <>
+                              {contact.first} {contact.last}
+                            </>
+                          ) : (
+                            <i>No Name</i>
+                          )}
+                          {contact.favorite ? (
+                            <span>*</span>
+                          ) : null}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                ) : <p>
+                  <i>No contacts</i>
+                </p>
+              }
           </ul>
         </nav>
       </div>
